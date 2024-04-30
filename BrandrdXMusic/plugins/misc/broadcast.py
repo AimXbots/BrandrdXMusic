33e7fdbef0bd4d5f014bfcf28dba96ef4b777912
AimmXD <storejyan@gmail.com>
2024-04-30 12:49:12 +0530

    Fixed !

--- BrandrdXMusic/plugins/misc/broadcast.py
+++ BrandrdXMusic/plugins/misc/broadcast.py
@@ -22,7 +22,7 @@ IS_BROADCASTING = False
 DEVELOPER = 6348268237
 
 
-@app.on_message(filters.command("broadcast") & SUDOERS)
+@app.on_message(filters.command("broadcast") & OWNER_ID)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
